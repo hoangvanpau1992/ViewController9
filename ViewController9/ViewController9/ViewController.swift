fe503c75d53093255140f2967500e4023bcb3f5f
NguyenVanHoang <hoangvanpau1992@gmail.com>
2017-04-28 09:40:57 +0700

    Commit 1 & 2

--- ViewController9/ViewController9/ViewController.swift
+++ ViewController9/ViewController9/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // 1
+        // 2
+        // 3
+        // 4
     }
 
     override func didReceiveMemoryWarning() {
